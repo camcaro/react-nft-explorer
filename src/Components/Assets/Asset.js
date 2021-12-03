51384a4ca48909908bed6fb56a3c319f46c75032
camcaro <carlos.camara.ortiz@gmail.com>
2021-12-03 12:36:11 +0100

    feat: show asset as a card (mui)

--- src/Components/Assets/Asset.js
+++ src/Components/Assets/Asset.js
@@ -1,3 +1,10 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
 const Asset = props => {
   const asset = props.asset;
   let content = <h2>Asset component</h2>;
@@ -5,16 +12,34 @@ const Asset = props => {
   if (asset.id) {
     // console.log(asset);
     content = (
-      <div>
-        <h2>Asset</h2>
-        <h3>Collection: {asset.collection.name}</h3>
-        <img src={asset.creator.profile_img_url} alt='Creator profile img' />
-        <p>Asset name: {asset.name}</p>
-        <img src={asset.image_url} alt={asset.name + ' image'} />
-        <p>Current Owner: {asset.owner.address}</p>
-        <h4>Description:</h4>
-        <p>Description: {asset.description}</p>
-      </div>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component='img'
+            height='140'
+            image={asset.image_url}
+            alt={asset.name + ' image'}
+          />
+          <CardContent>
+            <Typography gutterBottom variant='h5' component='div'>
+              {asset.name}
+            </Typography>
+            <Typography variant='body2' color='text.secondary'>
+              {asset.description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+      // <div>
+      //   <h2>Asset</h2>
+      //   <h3>Collection: {asset.collection.name}</h3>
+      //   <img src={asset.creator.profile_img_url} alt='Creator profile img' />
+      //   <p>Asset name: {asset.name}</p>
+      //   <img src={asset.image_url} alt={asset.name + ' image'} />
+      //   <p>Current Owner: {asset.owner.address}</p>
+      //   <h4>Description:</h4>
+      //   <p>Description: {asset.description}</p>
+      // </div>
     );
   }
 
